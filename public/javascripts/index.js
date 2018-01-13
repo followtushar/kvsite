2bfe62d37305a166c4a8030f0b8da24cde625a84
followtushar <followtushar@gmail.com>
2018-01-13 15:06:33 +0530

    Debuging

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,62 +3,7 @@ $(document).ready(function () {
     temperatureData = [],
     humidityData = [];
     
-//   // var data = {
-//   //   labels: timeData,
-//   //   datasets: [
-//   //     {
-//   //       fill: false,
-//   //       label: 'Temperature',
-//   //       yAxisID: 'Temperature',
-//   //       borderColor: "rgba(255, 204, 0, 1)",
-//   //       pointBoarderColor: "rgba(255, 204, 0, 1)",
-//   //       backgroundColor: "rgba(255, 204, 0, 0.4)",
-//   //       pointHoverBackgroundColor: "rgba(255, 204, 0, 1)",
-//   //       pointHoverBorderColor: "rgba(255, 204, 0, 1)",
-//   //       data: temperatureData
-//   //     },
-//   //     {
-//   //       fill: false,
-//   //       label: 'Humidity',
-//   //       yAxisID: 'Humidity',
-//   //       borderColor: "rgba(24, 120, 240, 1)",
-//   //       pointBoarderColor: "rgba(24, 120, 240, 1)",
-//   //       backgroundColor: "rgba(24, 120, 240, 0.4)",
-//   //       pointHoverBackgroundColor: "rgba(24, 120, 240, 1)",
-//   //       pointHoverBorderColor: "rgba(24, 120, 240, 1)",
-//   //       data: humidityData
-//   //     }
-//   //   ]
-//   // }
 
-//   // var basicOption = {
-//   //   title: {
-//   //     display: true,
-//   //     text: 'Temperature Real-time Data',
-//   //     fontSize: 36
-//   //   },
-//   //   scales: {
-//   //     yAxes: [{
-//   //       id: 'Temperature',
-//   //       type: 'linear',
-//   //       scaleLabel: {
-//   //         labelString: 'Temperature(C)',
-//   //         display: true
-//   //       },
-//   //       position: 'left',
-//   //     }, {
-//   //         id: 'Humidity',
-//   //         type: 'linear',
-//   //         scaleLabel: {
-//   //           labelString: '',
-//   //           display: true
-//   //         },
-//   //         position: 'right'
-//   //       }]
-//   //   }
-//   // }
-
-//   // Get the context of the canvas element we want to select
 //   // var ctx = document.getElementById("myChart").getContext("2d");
 //   // var optionsNoAnimation = { animation: false }
 //   // var myLineChart = new Chart(ctx, {
@@ -81,7 +26,7 @@ $(document).ready(function () {
       console.log(message.data);
       timeData.push(obj.time);
       temperatureData.push(obj.Weight);
-      $('#mydiv').html(obj.Weight);
+      $('#myDiv').html(obj.Weight);
       // only keep no more than 50 points in the line chart
       const maxLen = 50;
       var len = timeData.length;
